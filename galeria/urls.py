e20f508f955e2b0c353fdad4af599b6fa6fe1bd5
ByfRoots <byfroost8@gmail.com>
2024-02-02 19:52:30 +0000

    add a gallery and modifying somethings

---
+++ galeria/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from galeria.views import index, imagem
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('imagem/', imagem, name='imagem')
+]
+
